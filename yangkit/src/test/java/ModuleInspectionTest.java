281bb81f0c2f5c2477056fc1654b2fe03679a7ea
mxyns <mx.yns@outlook.fr>
2026-04-09 17:26:21 +0800

    module inspection test

--- yangkit/src/test/java/ModuleInspectionTest.java
+++ yangkit/src/test/java/ModuleInspectionTest.java
@@ -1,24 +1,48 @@
 import org.dom4j.DocumentException;
 import org.junit.jupiter.api.Test;
+import org.yangcentral.yangkit.base.YangBuiltinKeyword;
+import org.yangcentral.yangkit.base.YangElement;
+import org.yangcentral.yangkit.common.api.QName;
 import org.yangcentral.yangkit.common.api.validate.ValidatorResult;
 import org.yangcentral.yangkit.model.api.schema.YangSchemaContext;
+import org.yangcentral.yangkit.model.api.stmt.Module;
+import org.yangcentral.yangkit.model.api.stmt.YangStatement;
+import org.yangcentral.yangkit.parser.YangParser;
+import org.yangcentral.yangkit.parser.YangParserEnv;
 import org.yangcentral.yangkit.parser.YangParserException;
+import org.yangcentral.yangkit.parser.YangYinParser;
 import org.yangcentral.yangkit.xpath.impl.YangXPathImpl;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ModuleInspectionTest {
 
     @Test
-    void testLoadValidSchema() throws DocumentException, IOException, YangParserException {
+    void testInspectModuleVersion() throws IOException, YangParserException {
         String yang = "../yang/schema-test.yang";
-        YangSchemaContext schema = YangkitUtils.loadSchema(yang);
-        assertNotNull(schema);
-        ValidatorResult schemaValidation = YangkitUtils.validateSchema(schema);
-        assertTrue(schemaValidation.isOk());
-        String namespace = schema.getModule("schema-test").getFirst().getMainModule().getNamespace().getArgStr();
-        assertEquals("urn:schema:test", namespace);
+
+        // Initialize a parser like it's done in YinYangParser.parse
+        YangParser parser = new YangParser();
+        Path path = Path.of(yang);
+        String content = Files.readString(path);
+        YangParserEnv env = new YangParserEnv();
+        env.setCurPos(0);
+        env.setFilename(path.getFileName().toString());
+        env.setYangStr(content);
+
+        // Walk the AST
+        List<YangElement> elements = parser.parseYang(content, env);
+        for (YangElement el: elements) {
+            if (el instanceof Module module) {
+                YangStatement version = module.getSubStatement(YangBuiltinKeyword.YANGVERSION.getQName()).getFirst();
+                assertEquals("1.1", version.getArgStr());
+            }
+        }
     }
 }
